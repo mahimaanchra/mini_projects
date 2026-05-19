58152b53c925d4a7d2c3ef6c3d0f2161ad423b79
Mahima <mahimaanchra@gmail.com>
2026-05-19 18:23:58 +0530

    changed digital clock

--- practice/digitalcloack/d.js
+++ practice/digitalcloack/d.js
@@ -11,7 +11,7 @@ let ampmValue = hh>12? 'PM' : 'AM';
 ampm.innerHTML = ampmValue;
 let hstring = hh<10 ? `0${hh}` : hh;
 let sstring = ss<10 ? `0${ss}` : ss;
-let mstring = ss<10 ? `0${mm}` : mm;
+let mstring = mm<10 ? `0${mm}` : mm;
 
 display.innerHTML = `${hstring} : ${mstring} : ${sstring}`;
     }
